6b4b73f025498a229d29cd0f92e9da556908d916
просто человек <nuhajjuh@gmail.com>
2026-06-04 17:15:05 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,7 @@
+# файл convert.py - конвертирует видео в цветной формат YUV420
+
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2026 prostochelovek097
 import cv2
 import os
 import numpy as np
